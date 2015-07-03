07b08260062b229a1ec976c1400ce859e2dcd907
Nils-Helge Garli Hegvik <nilsga@gmail.com>
2015-07-03 09:57:15 +0200

    Added akka remote config

--- scala/src/main/java/no/bekk/trhjava/pi/web/Main.java
+++ scala/src/main/java/no/bekk/trhjava/pi/web/Main.java
@@ -1,11 +1,24 @@
 package no.bekk.trhjava.pi.web;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import no.bekk.trhjava.pi.actor.StringActor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
+    @Bean
+    public ActorSystem actorSystem() {
+        return ActorSystem.create("Akka-Pi");
+    }
+
+    @Bean
+    public ActorRef stringActor() {
+        return actorSystem().actorOf(StringActor.mkProps());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
